a4ffff831c10aa208311d955e60f22165109928e
AbrLis <abrli@yandex.ru>
2024-04-01 19:56:03 +0500

    Предварительные наброски структур распределителя вычислений

---
+++ pkg/calculator/types.go
@@ -0,0 +1,35 @@
+package calculator
+
+import (
+	"sync"
+	"time"
+)
+
+var done chan struct{} // Канал завершения вычислительных операций
+
+// Token - структура для формирования польской нотации выражения
+type Token struct {
+	Value string
+	IsOp  bool
+}
+
+// TaskCalculate - структура для формирования задачи
+type TaskCalculate struct {
+	ID         string  `json:"id"`
+	Expression []Token `json:"expression"`
+}
+
+// FreeCalculators - Структура счётчика свободных вычислителей
+type FreeCalculators struct {
+	Count           int                      // Количество вычислителей
+	CountFree       int                      // Свободные вычислители
+	PingTimeoutCalc []time.Time              // Таймауты пингов вычислителей
+	Queue           []TaskCalculate          // Очередь исполнения задач
+	queueInProcess  map[string]TaskCalculate // Задачи находящиеся на обработке
+	taskChannel     chan TaskCalculate       // Канал задач
+	AddTimeout      time.Duration            // Таймауты операций
+	SubtractTimeout time.Duration
+	MultiplyTimeout time.Duration
+	DivideTimeout   time.Duration
+	mu              sync.Mutex
+}
