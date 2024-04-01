1c3e2b2f5799eb9ab0ec6433b34e7bf589dfabe6
AbrLis <abrli@yandex.ru>
2024-04-01 10:22:32 +0500

    тестовые правки

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"GoComputeFlow/pkg/api"
+	"GoComputeFlow/pkg/database"
 	"time"
 )
 
 func main() {
+	_ = database.OpenDB()
 	api.StartServer()
 	time.Sleep(time.Hour)
 }
