fd7ee4dc384eefeb780faeb6fd67333820395fe4
AbrLis <abrli@yandex.ru>
2024-04-01 19:55:20 +0500

    добавлен id пользователя в токен при проверках авторизации

--- pkg/api/auth/middleware.auth.go
+++ pkg/api/auth/middleware.auth.go
@@ -23,7 +23,7 @@ func EnsureAuth() gin.HandlerFunc {
 		// Валидация токена
 		tokenJWT := jwtTokenString[1]
 
-		_, err := jwt.Parse(
+		tokenFromString, err := jwt.Parse(
 			tokenJWT, func(token *jwt.Token) (interface{}, error) {
 				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 					return nil, jwt.ErrSignatureInvalid
@@ -34,6 +34,20 @@ func EnsureAuth() gin.HandlerFunc {
 
 		if err != nil {
 			c.JSON(401, gin.H{"message": "Unauthorized: " + err.Error()})
+			c.Abort()
+			return
+		}
+
+		// Добавление id пользователя в контекст
+		if claims, ok := tokenFromString.Claims.(jwt.MapClaims); ok && tokenFromString.Valid {
+			if userId, ok := claims["user_id"]; ok {
+				c.Set("user_id", userId)
+			} else {
+				c.JSON(500, gin.H{"error": "Не найден ID пользователя в токене"})
+				c.Abort()
+			}
+		} else {
+			c.JSON(500, gin.H{"error": "Ошибка обработки jwt токена"})
 			c.Abort()
 			return
 		}
