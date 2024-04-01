4518a35aab94f150fd544f4dc1bc49f416ce5fee
AbrLis <abrli@yandex.ru>
2024-04-01 10:46:36 +0500

    Дописана функция middleware аутентификации пользователя.

--- pkg/api/auth/middleware.auth.go
+++ pkg/api/auth/middleware.auth.go
@@ -1,11 +1,41 @@
 package auth
 
 import (
+	"os"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
+
+var SECRETKEY = os.Getenv("SECRET_KEY")
 
 func ensureAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Next()
+		token := c.Request.Header.Get("Authorization")
+		jwtTokenString := strings.Split(token, " ")
+		if token == "" || len(jwtTokenString) != 2 {
+			c.JSON(401, gin.H{"message": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		// Валидация токена
+		tokenJWT := jwtTokenString[1]
+
+		_, err := jwt.Parse(
+			tokenJWT, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, jwt.ErrSignatureInvalid
+				}
+				return []byte(SECRETKEY), nil
+			},
+		)
+
+		if err != nil {
+			c.JSON(401, gin.H{"message": "Unauthorized: " + err.Error()})
+			c.Abort()
+			return
+		}
 	}
 }
