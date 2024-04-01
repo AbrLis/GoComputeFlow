639be2144686c36442111d62e7914689754f8fa7
AbrLis <abrli@yandex.ru>
2024-04-01 19:56:53 +0500

    Добавлен парсер перевода строки выражения в польскую нотацию

---
+++ pkg/calculator/utilites.go
@@ -0,0 +1,54 @@
+package calculator
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+// ParseExpression разбивает выражение на токены в польской нотации
+func ParseExpression(expr string) ([]Token, error) {
+	var tokens []Token
+	var ops []rune
+	var buffer string
+
+	for _, ch := range expr {
+		if unicode.IsDigit(ch) {
+			buffer += string(ch)
+		} else if strings.ContainsRune("+-*/", ch) {
+			if buffer != "" {
+				tokens = append(tokens, Token{Value: buffer, IsOp: false})
+				buffer = ""
+			}
+			for len(ops) > 0 && precedence(ops[len(ops)-1]) >= precedence(ch) {
+				tokens = append(tokens, Token{Value: string(ops[len(ops)-1]), IsOp: true})
+				ops = ops[:len(ops)-1]
+			}
+			ops = append(ops, ch)
+		} else if ch != ' ' {
+			return nil, fmt.Errorf("invalid character: %c", ch)
+		}
+	}
+
+	if buffer != "" {
+		tokens = append(tokens, Token{Value: buffer, IsOp: false})
+	}
+
+	for len(ops) > 0 {
+		tokens = append(tokens, Token{Value: string(ops[len(ops)-1]), IsOp: true})
+		ops = ops[:len(ops)-1]
+	}
+
+	return tokens, nil
+}
+
+func precedence(op rune) int {
+	switch op {
+	case '+', '-':
+		return 1
+	case '*', '/':
+		return 2
+	default:
+		return 0
+	}
+}
