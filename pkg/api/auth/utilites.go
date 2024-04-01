d2d12c05b3b03e023e6471ca7242c100e5603711
AbrLis <abrli@yandex.ru>
2024-04-01 10:22:16 +0500

    Исправлена логика проверки существующего пользователя

--- pkg/api/auth/utilites.go
+++ pkg/api/auth/utilites.go
@@ -13,20 +13,13 @@ type Error struct {
 
 // CheckUserExists проверяет, существует ли пользователь с указанным логином и паролем
 func CheckUserExists(login, password string) (bool, Error) {
-	hashedPasswordLogin, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	user, err := database.GetUser(login)
 	if err != nil {
-		return false, Error{Code: 500, Msg: gin.H{"error login": err.Error()}}
+		return false, Error{Code: 500, Msg: gin.H{"error login db error": err.Error()}}
 	}
-	user, err := database.GetUser(string(hashedPasswordLogin))
-
-	// Проверка, что такой пользователь зарегистрирован
-	if user.Login != login {
-		return false, Error{Code: 401, Msg: gin.H{"error": "Пользователь не зарегистрирован"}}
-	}
-
-	// Проверка пароля
-	if string(hashedPasswordLogin) != user.HashPassword {
-		return false, Error{Code: 401, Msg: gin.H{"error": "Неверный пароль"}}
+	err = bcrypt.CompareHashAndPassword([]byte(user.HashPassword), []byte(password))
+	if err != nil {
+		return false, Error{Code: 401, Msg: gin.H{"error password": err.Error()}}
 	}
 
 	return true, Error{}
