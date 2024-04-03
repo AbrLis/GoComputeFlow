4443f12629e94c4f62bb935c0a87517069ab28d4
AbrLis <abrli@yandex.ru>
2024-04-03 13:53:24 +0500

    открыт эндпоинт /get-operations"     для получения списка доступных операций со временем их выполнения

--- pkg/api/main.go
+++ pkg/api/main.go
@@ -19,7 +19,7 @@ func StartServer() {
 		apiRouters.POST(addExpressionPath, auth.EnsureAuth(), AddExpressionHandler)
 		apiRouters.GET(getExpressionsPath, auth.EnsureAuth(), GetExpressionsHandler)
 		apiRouters.GET(getValuePath, auth.EnsureAuth(), GetValueHandler)
-		apiRouters.GET(getOperationsPath, auth.EnsureAuth(), GetOperationsHandler)
+		apiRouters.GET(getOperationsPath, GetOperationsHandler)
 		//apiRouters.GET(monitoring, GetMonitoringHandler)
 	}
 
