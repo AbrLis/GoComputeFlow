c2255f2676aabd79e9073f47d1c67a66cf7fb870
AbrLis <abrli@yandex.ru>
2024-04-04 14:54:35 +0500

    Наброски gRPC сервера воркера

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.22
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/golang/protobuf v1.5.4
 	golang.org/x/crypto v0.21.0
+	google.golang.org/grpc v1.63.0
+	google.golang.org/protobuf v1.33.0
 	gorm.io/driver/sqlite v1.5.5
 	gorm.io/gorm v1.25.9
 )
@@ -35,6 +38,6 @@ require (
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/worker/server/main.go
@@ -0,0 +1,95 @@
+package worker
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/ptypes/empty"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"GoComputeFlow/pkg/worker"
+	pb "GoComputeFlow/pkg/worker/proto"
+)
+
+type Server struct {
+	pb.WorkerServiceServer
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+// StartServerWorker запускает gRPC-сервер
+func StartServerWorker(host, port string) error {
+	addr := fmt.Sprintf("%s:%s", host, port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	log.Printf("Start server gRPC worker on: %s:%s", addr, port)
+
+	grpcServer := grpc.NewServer()
+	myServiceDriver := NewServer()
+	pb.RegisterWorkerServiceServer(grpcServer, myServiceDriver)
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Println("Error start server gRPC worker: ", err)
+		return err
+	}
+	return nil
+}
+
+// GetTimeouts возвращает таймауты вычислений в текстовом виде
+func (s *Server) GetTimeouts(ctx context.Context, req *empty.Empty) (*pb.TimeoutsMessage, error) {
+	return &pb.TimeoutsMessage{
+		Add:      fmt.Sprintf("%.2f sec", worker.ADDTIMEOUT.Seconds()),
+		Subtract: fmt.Sprintf("%.2f sec", worker.SUBTRACTTIMEOUT.Seconds()),
+		Multiply: fmt.Sprintf("%.2f sec", worker.MULTIPLYTIMEOUT.Seconds()),
+		Divide:   fmt.Sprintf("%.2f sec", worker.DIVIDETIMEOUT.Seconds()),
+	}, nil
+}
+
+// SetTimeouts устанавливает таймауты вычислений
+func (s *Server) SetTimeouts(ctx context.Context, req *pb.TimeoutsRequest) (*empty.Empty, error) {
+	worker.DataWorker.AddTimeout = req.Add.AsDuration()
+	worker.DataWorker.SubtractTimeout = req.Subtract.AsDuration()
+	worker.DataWorker.MultiplyTimeout = req.Multiply.AsDuration()
+	worker.DataWorker.DivideTimeout = req.Divide.AsDuration()
+
+	return &empty.Empty{}, nil
+}
+
+// SetTask Добавляет задачу в очередь исполнения
+func (s *Server) SetTask(ctx context.Context, req *pb.TaskRequest) (*empty.Empty, error) {
+	expression := make([]worker.Token, len(req.Expression))
+	for i, token := range req.Expression {
+		expression[i] = worker.Token{
+			Value: token.Value,
+			IsOp:  token.IsOp,
+		}
+	}
+
+	worker.DataWorker.Queue = append(
+		worker.DataWorker.Queue, worker.TaskCalculate{
+			ID:         uint(req.UserId),
+			Expression: expression,
+		},
+	)
+
+	return &empty.Empty{}, nil
+}
+
+// GetResult возвращает результат вычисления
+func (s *Server) GetResult(ctx context.Context, req *empty.Empty) (*pb.TaskRespons, error) {
+	if worker.DataWorker.ResultQueue == nil || len(worker.DataWorker.ResultQueue) == 0 {
+		return nil, fmt.Errorf("no results")
+	}
+
+	worker.DataWorker.Mu.Lock()
+	data := worker.DataWorker.ResultQueue[0]
+	worker.DataWorker.ResultQueue = worker.DataWorker.ResultQueue[1:]
+	worker.DataWorker.Mu.Unlock()
+
+	return &pb.TaskRespons{UserId: int32(data.ID), Value: float32(data.Result), FlagError: data.FlagError}, nil
+}
