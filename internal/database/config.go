0b61da7f73afb10afb2dfc298727392c3c44b052
AbrLis <abrli@yandex.ru>
2024-04-07 12:40:52 +0500

    Добавлены константы таймаутов по умолчанию

--- internal/database/config.go
+++ internal/database/config.go
@@ -1,5 +1,9 @@
 package database
 
+import (
+	"time"
+)
+
 const (
 	StatusInProgress TaskStatus = "In progress"
 	StatusCompleted  TaskStatus = "Completed"
@@ -7,3 +11,11 @@ const (
 )
 
 type TaskStatus string
+
+// Константы таймаутов вычислений по умолчанию
+const (
+	ADDTIMEOUT      = 5 * time.Second
+	SUBTRACTTIMEOUT = 3 * time.Second
+	MULTIPLYTIMEOUT = 4 * time.Second
+	DIVIDETIMEOUT   = 6 * time.Second
+)
