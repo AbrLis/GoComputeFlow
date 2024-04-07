24f9293fe8828d8df7a7ac8e90a45c9d9ded87dd
AbrLis <abrli@yandex.ru>
2024-04-07 12:35:20 +0500

    добавлена модель хранения таймаутов в бд

--- internal/database/models.go
+++ internal/database/models.go
@@ -12,6 +12,7 @@ type InfoModel struct {
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
+// User Таблица пользователя
 type User struct {
 	InfoModel
 	Login        string
@@ -19,10 +20,20 @@ type User struct {
 	Expressions  []Expression `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
+// Expression Таблица представления выражений
 type Expression struct {
 	InfoModel
 	UserId     uint `json:"-"`
 	Expression string
 	Result     string
 	Status     TaskStatus
+}
+
+// Timeouts таблица для сохранения таймаутов воркеров между перезапусками
+type Timeouts struct {
+	InfoModel
+	AddTimeout      time.Duration
+	SubtractTimeout time.Duration
+	MutiplyTimeout  time.Duration
+	DivideTimeout   time.Duration
 }
