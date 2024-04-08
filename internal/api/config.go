f4f4447629bb1ea48e876e2ba12d6550d1f22687
AbrLis <abrli@yandex.ru>
2024-04-08 12:17:59 +0500

    Исправление константы на будущее

--- internal/api/config.go
+++ internal/api/config.go
@@ -7,7 +7,7 @@ const (
 	getValuePath       = "/get-value/:task_id" // API для получения значения выражения по его идентификатору
 	getOperationsPath  = "/get-operations"     // API для получения списка доступных операций со временем их выполнения
 	setOperationsPath  = "/set-operations"     // API для установки времени выполнения операции
-	monitoring         = "/monitoring"         // API для получения статуса вычислителей (времени последнего пинга)
+	monitoringPath     = "/monitoring"         // API для получения статуса вычислителей (времени последнего пинга)
 	HostPath           = "localhost"           // Путь до хоста
 	PortHost           = ":3000"               // Порт хоста
 	registerPath       = "/register"           // API для регистрации пользователя
