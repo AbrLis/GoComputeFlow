381ce8e7dd3a038381c51768a2bf5e0b8631ec10
AbrLis <abrli@yandex.ru>
2024-04-15 11:03:10 +0500

    При выводе главной страницы добавлены данных для таблицы результатов мелкие правки

--- internal/frontend/handlers.go
+++ internal/frontend/handlers.go
@@ -5,16 +5,20 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
+	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 
 	"GoComputeFlow/internal/api"
+	"GoComputeFlow/internal/database"
 )
 
 type tokenJWT struct {
-	Token string `json:"token"`
+	Token  string `json:"token"`
+	UserID string `json:"user_id"`
 }
 
 func render(c *gin.Context, templateName string, data gin.H) {
@@ -23,7 +27,21 @@ func render(c *gin.Context, templateName string, data gin.H) {
 
 func showIndexPage(c *gin.Context) {
 	// TODO: Запрос информации об операциях и её добавление в шаблон
-	render(c, "index.html", nil)
+	userID, exist := c.Get("user_id")
+	userIDUint, err := strconv.ParseUint(userID.(string), 10, 64)
+	if err != nil {
+		log.Println("Ошибка преобразования user_id: ", err)
+		c.Redirect(http.StatusFound, "/login")
+		c.Abort()
+	}
+	var data []database.Expression
+	if exist {
+		data, _ = database.GetNTasks(uint(userIDUint), 10)
+	}
+	render(c, "index.html", gin.H{
+		"expressions":  data,
+		"is_logged_in": true,
+	})
 }
 
 func showLoginPage(c *gin.Context) {
@@ -50,24 +68,25 @@ func performLogin(c *gin.Context) {
 	// обращение к API регистрации
 	request, _ := http.NewRequest("POST", APIPath+"/register", bytes.NewReader(data))
 	request.Header.Set("Content-Type", "application/json")
-	resp, err := http.DefaultClient.Do(request)
-	resp.Body.Close()
+	if resp, err := http.DefaultClient.Do(request); err == nil {
+		resp.Body.Close()
+	}
 
 	// обращение к API логина
 	request, _ = http.NewRequest("POST", APIPath+"/login", bytes.NewReader(data))
 	request.Header.Set("Content-Type", "application/json")
-	resp, err = http.DefaultClient.Do(request)
+	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
 		errorLogin(c, "Error", err)
 		return
 	}
+	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		errorLogin(c, "Error", err)
 		return
 	}
-	resp.Body.Close()
 	var token tokenJWT
 	err = json.Unmarshal(body, &token)
 	if err != nil {
@@ -76,8 +95,9 @@ func performLogin(c *gin.Context) {
 	}
 
 	// Логин успешно пройден, установить куку и перейти на глвную
+
 	c.SetCookie("jwt_key", token.Token, int(time.Hour*24), "/", "", false, true)
-	c.Set("is_logged_in", true)
+	c.SetCookie("user_id", token.UserID, int(time.Hour*24), "/", "", false, true)
 	c.Redirect(302, "/")
 }
 
