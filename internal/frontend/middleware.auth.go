0c08d36e93a26ceb75520dfe8eb7e7857db4581e
AbrLis <abrli@yandex.ru>
2024-04-15 15:59:40 +0500

    Исправлена логика авторизации

--- internal/frontend/middleware.auth.go
+++ internal/frontend/middleware.auth.go
@@ -7,17 +7,17 @@ import (
 
 func loggedIn() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		setLoggedIn := func(key, value string) {
+		setLoggedIn := func(key string) {
 			if val, err := c.Cookie(key); err == nil || val != "" {
-				c.Set(value, val)
+				c.Set(key, val)
 			} else {
 				c.Set("is_logged_in", false)
 				c.Redirect(http.StatusFound, "/login")
 				c.Abort()
 			}
 		}
-		setLoggedIn("jwt_key", "is_logged_in")
-		setLoggedIn("user_id", "user_id")
+		setLoggedIn("jwt_key")
+		setLoggedIn("user_id")
 		c.Set("is_logged_in", true)
 	}
 }
