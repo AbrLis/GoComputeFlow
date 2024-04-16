639819819fc1e9087973aeaebc1c819f87a70b63
AbrLis <abrli@yandex.ru>
2024-04-16 10:52:11 +0500

    Добавлен обработчик данных из формы

--- internal/frontend/routes.go
+++ internal/frontend/routes.go
@@ -2,6 +2,7 @@ package frontend
 
 func initializeRoutes() {
 	frontendRoute.GET("/", loggedIn(), showIndexPage)
+	frontendRoute.POST("/", loggedIn(), addExpression)
 	frontendRoute.GET("/login", showLoginPage)
 	frontendRoute.POST("/login", performLogin)
 	frontendRoute.GET("/logout", logOut)
