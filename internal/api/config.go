c07d8bd471e5f5f16e1c0800c7ac073c95fed742
AbrLis <abrli@yandex.ru>
2024-04-16 13:33:39 +0500

    Удалён ненужный теперь эндпоинт

--- internal/api/config.go
+++ internal/api/config.go
@@ -1,18 +1,17 @@
 package api
 
 const (
-	apiVersion          = "api/v1"
-	addExpressionPath   = "/add-expression"         // API для добавления арифметического выражения
-	getExpressionsPath  = "/get-expressions"        // API для получения списка арифметических выражений
-	getNExpressionsPath = "/get-expressions/:limit" // API для получения списка последних N арифметических выражений
-	getValuePath        = "/get-value/:task_id"     // API для получения значения выражения по его идентификатору
-	getOperationsPath   = "/get-operations"         // API для получения списка доступных операций со временем их выполнения
-	setOperationsPath   = "/set-operations"         // API для установки времени выполнения операции
-	monitoringPath      = "/monitoring"             // API для получения статуса вычислителей (времени последнего пинга)
-	HostPath            = "0.0.0.0"                 // Путь до хоста
-	PortHost            = ":3000"                   // Порт хоста
-	registerPath        = "/register"               // API для регистрации пользователя
-	LoginPath           = "/login"                  // API для входа пользователя
+	apiVersion         = "api/v1"
+	addExpressionPath  = "/add-expression"     // API для добавления арифметического выражения
+	getExpressionsPath = "/get-expressions"    // API для получения списка арифметических выражений
+	getValuePath       = "/get-value/:task_id" // API для получения значения выражения по его идентификатору
+	getOperationsPath  = "/get-operations"     // API для получения списка доступных операций со временем их выполнения
+	setOperationsPath  = "/set-operations"     // API для установки времени выполнения операции
+	monitoringPath     = "/monitoring"         // API для получения статуса вычислителей (времени последнего пинга)
+	HostPath           = "0.0.0.0"             // Путь до хоста
+	PortHost           = ":3000"               // Порт хоста
+	registerPath       = "/register"           // API для регистрации пользователя
+	LoginPath          = "/login"              // API для входа пользователя
 )
 
 const (
