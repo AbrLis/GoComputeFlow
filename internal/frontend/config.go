bd7e543bf4b3796f9216c5b1c4f90db119d6b2c6
AbrLis <abrli@yandex.ru>
2024-04-17 13:03:58 +0500

    добавлена ошибка парсинга

--- internal/frontend/config.go
+++ internal/frontend/config.go
@@ -1,6 +1,10 @@
 package frontend
 
+import "errors"
+
 const (
 	APIPath         = "http://localhost:3000/api/v1"
 	CountExpression = "10" // Количество выражений на главной странице
 )
+
+var errorsTimeout = errors.New("timeouts parsing error")
