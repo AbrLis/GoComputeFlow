985aa6968fd7f1bed3ef5cb51eb2ce9ef609a4f9
AbrLis <abrli@yandex.ru>
2024-04-18 19:02:43 +0500

    В шаблоны добавлена функция add для вычисления номеров страниц прямо в шаблоне

--- internal/frontend/main.go
+++ internal/frontend/main.go
@@ -2,6 +2,7 @@ package frontend
 
 import (
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"log"
 )
 
@@ -9,10 +10,18 @@ var frontendRoute *gin.Engine
 
 func StartFront() {
 	frontendRoute = gin.Default()
+	html := template.New("").Funcs(template.FuncMap{
+		"add": add,
+	})
+	html, err := html.ParseGlob("internal/frontend/templates/*")
+	if err != nil {
+		log.Fatalf("Failed to parse templates: %v", err)
+	}
+	frontendRoute.SetHTMLTemplate(html)
+
 	frontendRoute.Static("/static", "internal/frontend/static/")
-	frontendRoute.LoadHTMLGlob("internal/frontend/templates/*")
 	initializeRoutes()
-	err := frontendRoute.Run(":8080")
+	err = frontendRoute.Run(":8080")
 	log.Println("Frontend server started on port 8080")
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
